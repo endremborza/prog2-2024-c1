b66df323450f6954b4ddb494ac476887edd78427
DanielBarabas <barabas.daniel1@gmail.com>
2024-02-23 13:31:24 +0100

    first_try

--- solution-2/src.py
+++ solution-2/src.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
 
         diffs = (df[["x", "y"]] - row).pipe(lambda df: df**2).sum(axis=1)
         out.append({"dist": diffs.min(), "weapon": df.loc[diffs.argmin(), "weapon"]})
-    
     pd.DataFrame(out).to_csv("out.csv", index=False)
