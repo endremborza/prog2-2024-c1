543d921ad9ee4ce27bf0dc50ba35be1d55457296
DanielBarabas <barabas.daniel1@gmail.com>
2024-02-23 18:10:51 +0100

    k-d tree first implementation

---
+++ k-d_tree/preproc.py
@@ -0,0 +1,6 @@
+"""
+import pandas as pd
+
+pd.read_csv("input.csv").to_pickle("input.pickle")
+pd.read_csv("query.csv").to_pickle("query.pickle")
+"""
---
+++ k-d_tree/src.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from scipy import spatial
+
+
+df = pd.read_csv("input.csv")
+query_df = pd.read_csv("query.csv")
+
+tree = spatial.KDTree(df[["x","y"]])
+out = []
+for idx, row in query_df.iterrows():
+    out.append({"dist": tree.query(row)[0], "weapon": df.loc[tree.query(row)[1], "weapon"]})
+pd.DataFrame(out).to_csv("notebook_results/out_tree.csv", index=False)
