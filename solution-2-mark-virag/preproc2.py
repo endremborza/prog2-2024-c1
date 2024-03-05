28a40b2ea35a1d697f22030319ab4da64b21ab5f
mendolmark <mendolmark1998@gmail.com>
2024-03-05 18:38:25 +0100

    2nd solution

---
+++ solution-2-mark-virag/preproc2.py
@@ -0,0 +1,3 @@
+import pandas as pd
+pd.read_csv("input.csv").to_parquet("input.parquet")
+
---
+++ solution-2-mark-virag/src2.py
@@ -0,0 +1,21 @@
+import polars as pl
+from scipy import spatial
+
+
+query_df = pl.read_csv("query.csv").to_pandas()  
+df = pl.read_parquet("input.parquet").to_pandas()
+
+
+tree = spatial.cKDTree(df.iloc[:, [1, 2]])
+
+
+distances, indices = tree.query(query_df.iloc[:, [0, 1]], k=1)
+
+
+out_df = pl.DataFrame({
+    "dist": distances,
+    "weapon": df.iloc[indices, df.columns.get_loc("weapon")].values
+})
+
+out_df.write_csv("out.csv")
+
